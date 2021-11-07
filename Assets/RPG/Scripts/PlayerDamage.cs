3938b263b2ec97db8847ecd4a6c42ed7d9ffd505
juicyj201 <220102473@mycput.ac.za>
2021-11-07 18:05:23 +0200

    Add updates

--- Assets/RPG/Scripts/PlayerDamage.cs
+++ Assets/RPG/Scripts/PlayerDamage.cs
@@ -24,7 +24,7 @@ public class PlayerDamage : MonoBehaviour
     public GameObject midnightAttack;
     public GameObject freezingAttack;
 
-    public Canvas dialogueCanvas;
+    public Image enemyHealthBar1;
 
     private int counter;
     private bool colliding;
@@ -50,119 +50,138 @@ public class PlayerDamage : MonoBehaviour
         Vector2 pos = new Vector2();
         pos = player.transform.position;
 
-        if (dialogueCanvas.enabled == false)
+        if (enemyHealthBar1.enabled == false)
         {
+            if (Time.time > rand)
+            {
 
-            flameLashAttack.transform.position = pos;
-            flameLashAttack.GetComponent<Animator>().SetBool("Attacking", true);
-            flameLashAttack.GetComponent<SpriteRenderer>().enabled = true;
+                flameLashAttack.transform.position = pos;
+                flameLashAttack.GetComponent<Animator>().SetBool("Attacking", true);
+                flameLashAttack.GetComponent<SpriteRenderer>().enabled = true;
 
-            midnightAttack.transform.position = pos;
-            midnightAttack.GetComponent<Animator>().SetBool("Attacking", true);
-            midnightAttack.GetComponent<SpriteRenderer>().enabled = true;
+                midnightAttack.transform.position = pos;
+                midnightAttack.GetComponent<Animator>().SetBool("Attacking", true);
+                midnightAttack.GetComponent<SpriteRenderer>().enabled = true;
 
-            freezingAttack.transform.position = pos;
-            freezingAttack.GetComponent<Animator>().SetBool("Attacking", true);
-            freezingAttack.GetComponent<SpriteRenderer>().enabled = true;
-
+                freezingAttack.transform.position = pos;
+                freezingAttack.GetComponent<Animator>().SetBool("Attacking", true);
+                freezingAttack.GetComponent<SpriteRenderer>().enabled = true;
+            }
         }
+
+        /**
+        if(Input.GetButton("Fire3")){
+            counter += 1;
+        }
+        **/
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        if (collision.gameObject.CompareTag("Player") && Input.GetButton("Fire3"))
         {
-            colliding = true;
-            counter += 1;
+            CounterCheck();
 
-            if (counter == 2)
-            {
-                fullHealth5.enabled = false;
-                halfHealth5.enabled = true;
-            }
-            if (counter == 4)
-            {
-                fullHealth5.enabled = false;
-                halfHealth5.enabled = false;
-            }
-            if (counter == 6)
-            {
-                fullHealth4.enabled = false;
-                halfHealth4.enabled = true;
-            }
-            if (counter == 8)
-            {
-                fullHealth4.enabled = false;
-                halfHealth4.enabled = false;
-            }
-            if (counter == 10)
-            {
-                fullHealth3.enabled = false;
-                halfHealth3.enabled = true;
-            }
-            if (counter == 12)
-            {
-                fullHealth3.enabled = false;
-                halfHealth3.enabled = false;
-            }
-            if (counter == 14)
-            {
-                fullHealth2.enabled = false;
-                halfHealth2.enabled = true;
-            }
-            if (counter == 16)
-            {
-                fullHealth2.enabled = false;
-                halfHealth2.enabled = false;
-            }
-            if (counter == 18)
-            {
-                fullHealth1.enabled = false;
-                halfHealth1.enabled = true;
-            }
-            if (counter == 20)
-            {
-                fullHealth1.enabled = false;
-                halfHealth1.enabled = false;
-            }
+        }
+    }
 
-            if (boss.gameObject.CompareTag("EvilKing"))
+    public void CounterCheck() {
+        if (counter == 2)
+        {
+            fullHealth5.enabled = false;
+            halfHealth5.enabled = true;
+        }
+        if (counter == 4)
+        {
+            fullHealth5.enabled = false;
+            halfHealth5.enabled = false;
+        }
+        if (counter == 6)
+        {
+            fullHealth4.enabled = false;
+            halfHealth4.enabled = true;
+        }
+        if (counter == 8)
+        {
+            fullHealth4.enabled = false;
+            halfHealth4.enabled = false;
+        }
+        if (counter == 10)
+        {
+            fullHealth3.enabled = false;
+            halfHealth3.enabled = true;
+        }
+        if (counter == 12)
+        {
+            fullHealth3.enabled = false;
+            halfHealth3.enabled = false;
+        }
+        if (counter == 14)
+        {
+            fullHealth2.enabled = false;
+            halfHealth2.enabled = true;
+        }
+        if (counter == 16)
+        {
+            fullHealth2.enabled = false;
+            halfHealth2.enabled = false;
+        }
+        if (counter == 18)
+        {
+            fullHealth1.enabled = false;
+            halfHealth1.enabled = true;
+        }
+        if (counter == 20)
+        {
+            fullHealth1.enabled = false;
+            halfHealth1.enabled = false;
+        }
+
+        if (boss.gameObject.CompareTag("EvilKing"))
+        {
+            if (counter == 12 || counter > 12)
             {
-                if (counter == 12 || counter > 12)
-                {
-                    DestroyObject(player);
+                DestroyObject(player);
 
-                    gameOver.enabled = true;
-                }
+                gameOver.enabled = true;
             }
+        }
 
-            if (boss.gameObject.CompareTag("DarkSage"))
+        if (boss.gameObject.CompareTag("DarkSage"))
+        {
+            if (counter == 16 || counter > 16)
             {
-                if (counter == 16 || counter > 16)
-                {
-                    DestroyObject(player);
+                DestroyObject(player);
 
-                    gameOver.enabled = true;
-                }
+                gameOver.enabled = true;
             }
+        }
 
-            if (boss.gameObject.CompareTag("Atropos"))
+        if (boss.gameObject.CompareTag("Atropos"))
+        {
+            if (counter == 20 || counter > 20)
             {
-                if (counter == 20 || counter > 20)
-                {
-                    DestroyObject(player);
+                DestroyObject(player);
 
-                    gameOver.enabled = true;
-                }
+                gameOver.enabled = true;
             }
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player") && Input.GetButton("Fire3")) {
+            //CounterCheck();
         }
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
-        {	
-            colliding = false;
+        {
+            flameLashAttack.GetComponent<SpriteRenderer>().enabled = false;
+            midnightAttack.GetComponent<SpriteRenderer>().enabled = false;
+            freezingAttack.GetComponent<SpriteRenderer>().enabled = false;
         } 
     }
 }
