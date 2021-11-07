80848824438223302e51f43314445590fc703205
CharlesLemmert <81480730+CharlesLemmert@users.noreply.github.com>
2021-11-07 20:45:11 +0200

    Committing changes made

--- Assets/RPG/Scripts/buttonFunctions.cs
+++ Assets/RPG/Scripts/buttonFunctions.cs
@@ -1,12 +1,33 @@
+using System.IO;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class buttonFunctions : MonoBehaviour
 {
+    private static string LevelReachedOne = @"Assets\CurrentLevel.txt";
+    private string[] valueOne = File.ReadAllLines(LevelReachedOne);
+
+    public string levelName;
     public void startGame()
     {
-        //open story scene in here
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        int currentLevelValueOne = int.Parse(valueOne[0]);
+
+        if (currentLevelValueOne == 1)
+        {
+
+
+            //open story scene in here
+            SceneManager.LoadScene(levelName);
+            Debug.Log("The start worked");
+
+
+        }
+        else if (currentLevelValueOne == 0)
+        {
+            //open story scene in here
+            SceneManager.LoadScene("StoryIntro");
+
+        }
     }
     public void optionMenu()
     {
